e593653598090375c9a4dde6827b48d78a10b09a
BeaLydh <beatrice-olsson@hotmail.com>
2026-03-26 16:58:45 +0100

    bug och koppling till redis.io

--- config/redis.js
+++ config/redis.js
@@ -1,11 +1,14 @@
-import { connect } from "mongoose";
+import dontenv from 'dotenv';
 import { createClient } from "redis";
-
-const client = createClient();
+dontenv.config();
+const client = createClient({
+    url: process.env.REDIS_URL
+});
 
 //Startar och kör Redis en gång och skickar error om fel.
-client.on('error', (err) =>
-console.error('Redis-fel: ', err));
+client.on('error', (err) => {
+    console.error('Redis-fel: ', err);
+});
 
 //await client.connect() skappar error om inte länkad till container som kör.
 await client.connect();
